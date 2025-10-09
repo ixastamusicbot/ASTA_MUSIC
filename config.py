de43e6a37d84eea89edd9dcee8f5aff373d76ed7
ixastamusicbot <astakhan63@gmail.com>
2025-10-09 17:12:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/laiba-music/Laiba_music",
+    "https://github.com/ixastamusicbot/ASTA_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
