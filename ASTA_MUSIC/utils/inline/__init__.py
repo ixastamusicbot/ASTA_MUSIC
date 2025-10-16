a77c54efc48d972f5423836785f8afe19511a7ff
⋏ 𝛅 𝛕 ⋏ <astakhan63@gmail.com>
2025-10-17 02:27:15 +0530

    Update __init__.py

--- ASTA_MUSIC/utils/inline/__init__.py
+++ ASTA_MUSIC/utils/inline/__init__.py
@@ -8,8 +8,19 @@ from .start import *
 from .sudolist import *
 
 # Fix for telegram_markup import errors
-from .play import stream_markup, stream_markup_timer, close_markup
+from .play import stream_markup, stream_markup_timer
 
 # Aliases for backward compatibility
 telegram_markup = stream_markup
 telegram_markup_timer = stream_markup_timer
+
+# Dummy close_markup if not present in play.py
+try:
+    from .play import close_markup
+except ImportError:
+    from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+    def close_markup(_):
+        return InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="✖ Close", callback_data="close")]]
+        )
