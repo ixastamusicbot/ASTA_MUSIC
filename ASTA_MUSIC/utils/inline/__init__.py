bedbbe29bcdca10bf7d47752da7a1ebaee3b300a
⋏ 𝛅 𝛕 ⋏ <astakhan63@gmail.com>
2025-10-17 02:23:12 +0530

    Update __init__.py

--- ASTA_MUSIC/utils/inline/__init__.py
+++ ASTA_MUSIC/utils/inline/__init__.py
@@ -6,3 +6,10 @@ from .settings import *
 from .speed import *
 from .start import *
 from .sudolist import *
+
+# Fix for telegram_markup import errors
+from .play import stream_markup, stream_markup_timer, close_markup
+
+# Aliases for backward compatibility
+telegram_markup = stream_markup
+telegram_markup_timer = stream_markup_timer
