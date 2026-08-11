6fc03d828e04b6115c5c5adccad167b2fac92593
⋏ 𝛅 𝛕 ⋏ <astakhan63@gmail.com>
2026-08-12 03:26:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,9 +41,14 @@ PRIVACY_LINK = getenv("PRIVACY_LINK", "https://graph.org/PRIVACY-FOR-TEAM-PURVI-
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBotsb069e5') # youtube song api key
+API_URL = getenv("API_URL", "https://pvtz.nexgenbots.xyz")
+
+
+VIDEO_API_URL = getenv("VIDEO_API_URL", "https://api.video.nexgenbots.xyz")
+
+
+API_KEY = getenv("API_KEY", "NxGBNexGenBots1b1233")
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/ixastamusicbot/ASTA_MUSIC",
